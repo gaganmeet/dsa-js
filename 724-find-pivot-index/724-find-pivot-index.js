d50e4b2bd3b1ead76f5ca31835997e3bcc5c509a
Gaganmeet Bahri <gaganmeet.bahri96@gmail.com>
2022-06-25 09:59:45 +0530

    Time: 92 ms (78.76%), Space: 47.2 MB (17.49%) - LeetHub

---
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var pivotIndex = function(nums) {
+    let leftSum = [nums[0]]
+    let rightSum = new Array(nums.length)
+    rightSum[nums.length-1] = nums[nums.length-1]
+    for(let i=1;i<nums.length;i++){
+        leftSum.push(nums[i]+leftSum[i-1])
+    }
+    for(let i=nums.length-2;i>-1;i--){
+        rightSum[i] = nums[i]+rightSum[i+1]
+    }
+    let i = 0
+    let j = nums.length-1
+    while(i<nums.length){
+        if(leftSum[i]==rightSum[i])
+            return i
+        i++
+    }
+    console.log(leftSum, rightSum)
+    return -1
+};
